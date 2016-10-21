4e0e1d907c4a2e503980b9188e053a6f65072f86
SETI <ahcarvalho01@gmail.com>
2016-10-21 19:39:10 -0200

    criando contador recursivo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
+void contRecursivo(int i){
+	if(i<0)return;
+	cout<<i<<" ";
+	contRecursivo(i-1);
+}
 int main(){
 	cout<<"Meu deus isso até que é legal";
+	contRecursivo(10);
 	return 0;
 }
